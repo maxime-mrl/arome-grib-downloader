95f1dabde338eb627f508938ed2f4638613f28c5
maxime-mrl <maxitrompette@ymail.com>
2025-06-07 07:55:03 +0000

    added function for both cog and hdf to loop only once (about 10-20% faster)

--- app/grib_tools.py
+++ app/grib_tools.py
@@ -8,6 +8,7 @@ import h5py
 from dateutil import parser
 from universal_downloads import Downloader
 from typing import Optional, List, TypedDict
+import time
 
 class BandMetadata(TypedDict):
   element: str
@@ -66,6 +67,7 @@ class GribTools:
     """
     Helper function to download and process data automatically
     """
+    start = time.time()
     self.downloader.download()
     # check if downloader has downloaded the file correctly (only one file which exists)
     if len(self.downloader.files) != 1:
@@ -74,6 +76,7 @@ class GribTools:
     if not os.path.exists(file):
       raise Exception("Downloaded file does not exist")
     print(f"Downloaded file: {file}")
+    downloading_time = time.time() - start
     # check if we need to regrid the file
     try:
       ds = gdal.Open(file)
@@ -85,11 +88,19 @@ class GribTools:
     # process the downloaded file
     if ("list" in self.output_formats):
       self.list_metadata(file)
-    if ("cog" in self.output_formats):
+    if ("cog" in self.output_formats and "hdf" in self.output_formats):
+      hdfcog_start = time.time()
+      self.to_cog_and_hdf(file, self.output_dir)
+    elif ("cog" in self.output_formats):
       self.grib_to_cog(file, self.output_dir)
-    if ("hdf" in self.output_formats):
+    elif ("hdf" in self.output_formats):
       self.grib_to_hdf(file, self.output_dir)
-    # ...
+
+    
+    print("download and process done")
+    print(f"Total time: {time.time() - start:.2f} seconds")
+    print(f"Downloading time: {downloading_time:.2f} seconds")
+    print(f"Grib to COG/HDF time: {time.time() - hdfcog_start:.2f} seconds")
   
   def get_band_metadata(
     self,
@@ -188,7 +199,7 @@ class GribTools:
 
     if not output_file:
       output_file = f"{os.path.splitext(grib_file)[0]}_reprojected.grib2"
-      
+
     if not resolution:
       resolution = self.resolution if self.resolution else 0.1  # Default resolution if not provided
     
@@ -205,6 +216,104 @@ class GribTools:
     subprocess.run(cmd, shell=True, check=True)
     print(f"Reprojection done : {output_file}")
     return output_file
+
+  def to_cog_and_hdf(
+    self,
+    input_grib: str,
+    output_dir: Optional[str]=None,
+  ):
+    """
+    Convert GRIB file to Cloud Optimized GeoTIFF (COG) and HDF5
+    @param input_grib: Path to input GRIB file
+    @param output_dir: Path to output directory
+    """
+    # open grib file
+    ds = gdal.Open(input_grib)
+    if ds is None:
+      raise Exception(f"Could not open {input_grib}")
+    
+    print(f"Processing {input_grib} to cog...")
+
+    # Get band information
+    info = gdal.Info(ds, format='json')
+    bands = info['bands']
+    # Create HDF5 file
+    with h5py.File(os.path.join(output_dir, f"{self.name}.h5"), "w") as hdf5_file:
+      # Loop through each band
+      for band_idx, band_info in enumerate(bands, 1):
+        try:
+          band = ds.GetRasterBand(band_idx)
+          if band is None:
+            raise Exception(f"Could not access band {band_idx}")
+          # Extract metadata
+          metadata = band_info['metadata']['']
+          safe_metadata = self.get_band_metadata(metadata)
+          var_name = safe_metadata['element']
+          units = safe_metadata['unit']
+          level = safe_metadata['level']
+          # ---- COG PROCESSING ----
+          # check if the band is wanted
+          if self.select_band(metadata, self.cog_parameters):
+            time_dir = os.path.join(
+              output_dir,
+              safe_metadata['valid_time'].split(":")[0] + "_00Z"
+            )
+            os.makedirs(time_dir, exist_ok=True)
+            output_file = f"{time_dir}/{var_name}_{level}.tif"
+            print(f"\nProcessing band {var_name}_{level} at time {safe_metadata['valid_time']}")
+            
+            translate_options = gdal.TranslateOptions(
+              bandList=[band_idx],
+              creationOptions=[
+                "BIGTIFF=YES",
+                "COMPRESS=DEFLATE",
+                "TILED=YES",
+                "BLOCKXSIZE=256",
+                "BLOCKYSIZE=256",
+              ]
+            )
+            
+            gdal.Translate(output_file, ds, options=translate_options)
+          # ---- HDF5 PROCESSING ----
+          # check if the band is wanted
+          if not self.select_band(metadata, self.hdf_parameters): continue
+
+          print(f"\nProcessing band {band_idx}/{len(bands)}")
+          print(f"Variable: {var_name}, Level: {level}, Units: {units}")
+
+          # Read band data as a NumPy array
+          band_data = band.ReadAsArray()
+
+          # Create dataset in HDF5 file
+          dataset_name = f"{var_name}_{level}_{band_idx}"
+          hdf5_dataset = hdf5_file.create_dataset(dataset_name, data=band_data, compression="gzip")
+
+          # Store metadata in the dataset attributes
+          hdf5_dataset.attrs["GRIB_ELEMENT"] = var_name
+          hdf5_dataset.attrs["GRIB_SHORT_NAME"] = level
+          hdf5_dataset.attrs["GRIB_UNIT"] = units
+        except Exception as e:
+          print(f"Error processing band {band_idx}: {str(e)}")
+          continue
+        finally:
+          band = None
+          gc.collect()
+
+      # Extract lat/lon information
+      subdatasets = ds.GetSubDatasets()
+      for name, desc in subdatasets:
+        if "latitude" in desc.lower():
+          lat_ds = gdal.Open(name)
+          lat_data = lat_ds.ReadAsArray()
+          hdf5_file.create_dataset("latitude", data=lat_data)
+          lat_ds = None
+        elif "longitude" in desc.lower():
+          lon_ds = gdal.Open(name)
+          lon_data = lon_ds.ReadAsArray()
+          hdf5_file.create_dataset("longitude", data=lon_data)
+          lon_ds = None
+    ds = None
+    
 
   def list_metadata(
       self,
